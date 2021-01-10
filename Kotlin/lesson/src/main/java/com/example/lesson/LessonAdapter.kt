9a3ea0ed69a3ffe7130ebc1fabc3d97aeefd9196
norman19882003 <zhanyue19882003@163.com>
2021-01-10 16:45:46 +0800

    1、将Java转换成Kotlin

--- Kotlin/lesson/src/main/java/com/example/lesson/LessonAdapter.kt
+++ Kotlin/lesson/src/main/java/com/example/lesson/LessonAdapter.kt
@@ -1,87 +1,67 @@
-package com.example.lesson;
+package com.example.lesson
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.core.BaseViewHolder
+import com.example.lesson.LessonAdapter.LessonViewHolder
+import com.example.lesson.entity.Lesson
+import java.util.*
 
-import com.example.core.BaseViewHolder;
-import com.example.lesson.entity.Lesson;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.LessonViewHolder> {
-    private List<Lesson> list = new ArrayList<>();
-
-    void updateAndNotify(List<Lesson> list) {
-        this.list = list;
-        notifyDataSetChanged();
+class LessonAdapter : RecyclerView.Adapter<LessonViewHolder>() {
+    private var list: List<Lesson> = ArrayList()
+    fun updateAndNotify(list: List<Lesson>) {
+        this.list = list
+        notifyDataSetChanged()
     }
 
-    @Override
-    public int getItemCount() {
-        return list.size();
+    override fun getItemCount(): Int {
+        return list.size
     }
 
-    @NonNull
-    @Override
-    public LessonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return LessonViewHolder.onCreate(parent);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LessonViewHolder {
+        return LessonViewHolder.onCreate(parent)
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull LessonViewHolder holder, int position) {
-        holder.onBind(list.get(position));
+    override fun onBindViewHolder(holder: LessonViewHolder, position: Int) {
+        holder.onBind(list[position])
     }
-
 
     /**
      * 静态内部类
      */
-    public static class LessonViewHolder extends BaseViewHolder {
-
-        LessonViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
-
-        @NonNull
-        private static LessonViewHolder onCreate(ViewGroup parent) {
-            return new LessonViewHolder(LayoutInflater
-                    .from(parent.getContext())
-                    .inflate(R.layout.item_lesson, parent, false));
-        }
-
-        void onBind(Lesson lesson) {
-            String date = lesson.getDate();
+    class LessonViewHolder internal constructor(itemView: View) : BaseViewHolder(itemView) {
+        fun onBind(lesson: Lesson) {
+            var date = lesson.getDate()
             if (date == null) {
-                date = "日期待定";
+                date = "日期待定"
             }
-            setText(R.id.tv_date, date);
-
-            setText(R.id.tv_content, lesson.getContent());
-
-            final Lesson.State state = lesson.getState();
+            setText(R.id.tv_date, date)
+            setText(R.id.tv_content, lesson.getContent())
+            val state = lesson.getState()
             if (state != null) {
-                setText(R.id.tv_state, state.stateName());
-                int colorRes = R.color.playback;
-                switch (state) {
-                    case PLAYBACK: {
+                setText(R.id.tv_state, state.stateName())
+                var colorRes = R.color.playback
+                colorRes = when (state) {
+                    Lesson.State.PLAYBACK -> {
+
                         // 即使在 {} 中也是需要 break 的。
-                        colorRes = R.color.playback;
-                        break;
+                        R.color.playback
                     }
-                    case LIVE:
-                        colorRes = R.color.live;
-                        break;
-                    case WAIT:
-                        colorRes = R.color.wait;
-                        break;
+                    Lesson.State.LIVE -> R.color.live
+                    Lesson.State.WAIT -> R.color.wait
                 }
-                final int backgroundColor = itemView.getContext().getColor(colorRes);
-                getView(R.id.tv_state).setBackgroundColor(backgroundColor);
+                val backgroundColor = itemView.context.getColor(colorRes)
+                getView<View>(R.id.tv_state)!!.setBackgroundColor(backgroundColor)
+            }
+        }
+
+        companion object {
+            fun onCreate(parent: ViewGroup): LessonViewHolder {
+                return LessonViewHolder(LayoutInflater
+                        .from(parent.context)
+                        .inflate(R.layout.item_lesson, parent, false))
             }
         }
     }
